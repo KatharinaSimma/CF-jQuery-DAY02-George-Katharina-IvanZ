0ac840533a7b8cbfc13c3060c3a3fe7c4dc6c5ac
Ivan Zivanovic <i.zivanovic01@gmail.com>
2020-10-06 15:04:59 +0200

    04

--- photoAlbum/script.js
+++ photoAlbum/script.js
@@ -6,17 +6,12 @@ console.table(familyAlbum);
 $(document).ready(function(){
 
 // add images to the html
-    for (var i=0; i<familyAlbum.length; i++){
+    for (let i=0; i<familyAlbum.length; i++){
 
         //var familyPhoto = familyAlbum[i].img;
         //console.log(familyPhoto);
-        $("#mainDiv").append(`<div class="flexItem"><img id="img${i}" src='${familyAlbum[i].img}'></div>`).append(`<br><span>Relation: ${familyAlbum[i].name}</span>`);
+        $("#mainDiv").append(`<figure class="flexItem"><img id="img${i}" src='${familyAlbum[i].img}'></figure>`).append(`<br><span>Relation: ${familyAlbum[i].name}</span>`);
         //console.log($("#img${i}"));
-     
-        
-    }
-
-    for (var i=0; i<familyAlbum.length; i++){
 
         if(familyAlbum[i].name == 'Child'){
             $(`#img${i}`).css("border", "blue 4px solid");
@@ -25,11 +20,30 @@ $(document).ready(function(){
             $(`#img${i}`).css("border", "pink 4px solid");
         }
         
+        $(`#img${i}`).on("click", function(event) {
+
+                //console.log(this.id)
+    
+                    $(this).after(`<figcaption>${familyAlbum[i].hobbies}</figcaption>`)});
+
+                //console.log()
+
     };
 
+
+
+        
+            
+            //for (let i=0; i<familyAlbum.length; i++){
+
+                //$("img").on("click", function(event) {
+
+                //console.log(this.id)
     
+                    //$//(this).after(`<figcaption>${familyAlbum[i].hobbies}</figcaption>`)});
 
+                //console.log()
 
-
+    //}
 
 })
